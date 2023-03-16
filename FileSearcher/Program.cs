91b9162cf7868dc815ddd67f0f315cb41d9a7faa
Muhammad Alam <muhammad.alam.97@outlook.com>
2023-03-16 19:34:33 +0000

    Add project files.

---
+++ FileSearcher/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+static String GetUserInput()
+{
+    Console.WriteLine("Enter a directory path");
+    var path = Console.ReadLine();
+    return path;
+}
+
+
+static void Run()
+{
+    var path = GetUserInput();
+
+    Directory.GetFiles(path);
+}
+
---
+++ Test/TestSearcherTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using FileSearcher;
+using NUnit.Framework;
+
+
+namespace Test
+{
+    public class TestSearcherTest
+    {
+       
+        [Test]
+        public void CanValidateDirectory()
+        {
+            var currentDirectory = Directory.GetCurrentDirectory();
+            Assert.IsNotNull(currentDirectory);
+        }
+    }
+}
